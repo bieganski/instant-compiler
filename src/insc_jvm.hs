f77ede85723da69c821f977ae5c2fe02f7245315
Matuesz <bieganski.m@wp.pl>
2019-10-19 17:35:18 +0200

    dummy working with jasmin and java, without Instant code insight yet

--- src/insc_jvm.hs
+++ src/insc_jvm.hs
@@ -1,13 +1,74 @@
 import AbsInstant
 
-import Data.Text
+import qualified Data.Text as T
 
-default_superclass = "java/lang/object"
+default_superclass = "java/lang/Object"
+main_signature = ".method public static main([Ljava/lang/String;)V"
+default_constructor = [".method public <init>()V",
+                       "  aload_0",
+                       "  invokenonvirtual java/lang/Object/<init>()V",
+                       "  return",
+                       ".end method"]
 
--- class name -> prolog string
-buildWrappingClass :: String -> String
-buildWrappingClass cname = ".class " ++ cname
+main_begin = [".method public static main([Ljava/lang/String;)V"]
+            
+main_end = ["invokevirtual java/io/PrintStream/println(I)V",
+            "return",
+            ".end method"]
+
+get_print_stream = "getstatic java/lang/System/out Ljava/io/PrintStream;"
+
+buildConstructor :: T.Text
+buildConstructor = buildLines default_constructor
+
+buildMainBegin :: T.Text
+buildMainBegin = buildLines main_begin
+
+buildMainEnd :: T.Text
+buildMainEnd = buildLines main_end
+
+buildText :: [T.Text] -> T.Text
+buildText ts = T.intercalate (T.pack "\n") ts
+
+buildLines :: [String] -> T.Text
+buildLines xs = T.intercalate (T.pack "\n") (map T.pack xs) 
+
+buildProlog :: String -> T.Text
+buildProlog classname = buildLines [cls, super] where
+  cls = ".class " ++ classname 
+  super = ".super " ++ default_superclass
+
+
+limitStack :: Integer -> T.Text
+limitStack n = T.pack $ ".limit stack " ++ (show n)
+
+limitLocals :: Integer -> T.Text
+limitLocals n = T.pack $ ".limit locals " ++ (show n)
+
+buildMainContent :: Program -> T.Text
+buildMainContent (Prog stmts) = T.pack "bipush 10"
+
+
+buildMain :: Program -> T.Text
+buildMain prog = buildText [buildMainBegin,
+                                   limitStack 1000,
+                                   limitLocals 1000,
+                                   T.pack get_print_stream,
+                                   buildMainContent prog,
+                                   buildMainEnd]
+
+
+emptyProgram :: Program
+emptyProgram = Prog []
+
+
+
+buildIR :: Program -> T.Text
+buildIR prog = buildText [buildProlog "PrzykladowaKlasa",
+                                  buildConstructor,
+                                  buildMain prog]
+
 
 main :: IO ()
 main = do
-  putStrLn $ buildWrappingClass "Lol"
+  putStrLn $ T.unpack $ buildIR emptyProgram
