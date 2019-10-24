85ca582946aa1574ef23334dd990cb16aa0d1c69
Matuesz <bieganski.m@wp.pl>
2019-10-24 14:53:07 +0200

    seems to be working fine

--- src/insc_jvm.hs
+++ src/insc_jvm.hs
@@ -66,6 +66,35 @@ buildProlog classname = buildLines [cls, super] where
   cls = ".class " ++ classname 
   super = ".super " ++ default_superclass
 
+stackNeeded :: Integer -> Integer -> Integer
+stackNeeded e1stack e2stack = if
+  e1stack == e2stack
+  then e1stack + 1
+  else max e1stack e2stack
+
+computeParentDepth :: Exp -> Exp -> Map.Map Exp Integer -> Integer
+computeParentDepth e1 e2 m = stackNeeded (m Map.! e1) (m Map.! e2)
+
+computeStackMap :: Exp -> Map.Map Exp Integer
+computeStackMap e = case e of
+  ExpAdd e1 e2 -> Map.insert e (computeParentDepth e1 e2 m) m where
+    m = Map.union (computeStackMap e1) (computeStackMap e2)
+  ExpSub e1 e2 -> Map.insert e (computeParentDepth e1 e2 m) m where
+    m = Map.union (computeStackMap e1) (computeStackMap e2)
+  ExpMul e1 e2 -> Map.insert e (computeParentDepth e1 e2 m) m where
+    m = Map.union (computeStackMap e1) (computeStackMap e2)
+  ExpDiv e1 e2 -> Map.insert e (computeParentDepth e1 e2 m) m where
+    m = Map.union (computeStackMap e1) (computeStackMap e2)
+  ExpLit _ -> Map.singleton e 1
+  ExpVar _ -> Map.singleton e 1
+
+
+computeNeededStackStmt :: Stmt -> Integer
+computeNeededStackStmt (SAss _ e) = (computeStackMap e) Map.! e
+computeNeededStackStmt (SExp e) = (computeStackMap e) Map.! e
+
+computeStackLimit :: Program -> Integer
+computeStackLimit (Prog stmts) = foldr max 0 (map computeNeededStackStmt stmts)
 
 limitStack :: Integer -> T.Text
 limitStack n = T.pack $ ".limit stack " ++ (show n)
@@ -93,23 +122,27 @@ modifyAssignVariable x (num, m) = case Map.lookup x m of
   Just _ -> (num, m)
 
 
-emptyProgram :: Program
-emptyProgram = Prog []
+computeBinaryOpIR :: Exp -> Exp -> Map.Map Exp Integer -> T.Text -> SState T.Text
+computeBinaryOpIR e1 e2 m opBytecode = do
+  t1 <- computeExpIR e1 m
+  t2 <- computeExpIR e2 m
+  if (m Map.! e1) > (m Map.! e2)
+    then return $ buildText [t1, t2, opBytecode]
+    else return $ buildText [t2, t1, opBytecode]
 
-computeBinaryOpIR :: Exp -> Exp -> T.Text -> SState T.Text
-computeBinaryOpIR e1 e2 opBytecode = do
-  t1 <- computeExpIR e1
-  t2 <- computeExpIR e2
-  return $ buildText [t1, t2, opBytecode]
-  
-computeExpIR :: Exp -> SState T.Text
-computeExpIR e = do
+    
+computeExpIR :: Exp -> Map.Map Exp Integer -> SState T.Text
+computeExpIR e m = do
   case e of
-    ExpAdd e1 e2 -> computeBinaryOpIR e1 e2 (T.pack "iadd")
-    ExpSub e1 e2 -> computeBinaryOpIR e1 e2 (T.pack "isub")
-    ExpMul e1 e2 -> computeBinaryOpIR e1 e2 (T.pack "imul")
-    ExpDiv e1 e2 -> computeBinaryOpIR e1 e2 (T.pack "idiv")
-    ExpLit n -> return $ T.pack $ "sipush " ++ (show n)
+    ExpAdd e1 e2 -> computeBinaryOpIR e1 e2 m (T.pack "iadd")
+    ExpSub e1 e2 -> computeBinaryOpIR e1 e2 m (T.pack "isub")
+    ExpMul e1 e2 -> computeBinaryOpIR e1 e2 m (T.pack "imul")
+    ExpDiv e1 e2 -> computeBinaryOpIR e1 e2 m (T.pack "idiv")
+    ExpLit n -> return $ T.pack $ (push n) ++ (show n) where
+      push n
+        | n <= 255 = "bipush "
+        | n <= 65535 = "sipush "
+        | otherwise = "ldc "
     ExpVar (Ident x) -> do
       m <- gets snd
       case Map.lookup x m of
@@ -121,12 +154,14 @@ computeStmtIR stmt = do
   case stmt of
     SAss (Ident x) e -> do
       modify (modifyAssignVariable x)
-      t1 <- computeExpIR e
+      let m = computeStackMap e
+      t1 <- computeExpIR e m
       val <- get
       let xnum = getVariableNum x val 
       return $ buildText [t1, T.pack $ "istore " ++ (show xnum)] 
     SExp e -> do
-      t1 <- computeExpIR e
+      let m = computeStackMap e
+      t1 <- computeExpIR e m
       return $ buildPrintable t1
 
 
@@ -135,15 +170,20 @@ computeProgramIR (Prog stmts) = do
   ts <- forM stmts computeStmtIR
   return $ buildText ts
 
-buildMainContentIR :: Program -> Val -> T.Text
-buildMainContentIR p v = evalState (computeProgramIR p) v
+-- returns content and number of used variables (for limitLocals)
+buildMainContentIR :: Program -> Val -> (T.Text, Integer)
+buildMainContentIR p v = (text, fromIntegral $ Map.size $ snd state) where
+  (text, state) = runState (computeProgramIR p) v
 
+-- limits for locals and stack increased by 1 because of 'getstatic' usage
+-- and string argument in main function
 buildMain :: Program -> T.Text
 buildMain prog = buildText [buildMainBegin,
-                                   limitStack 1000,
-                                   limitLocals 1000,
-                                   buildMainContentIR prog val0,
-                                   buildMainEnd]
+                                   limitStack $ 1 + (computeStackLimit prog),
+                                   limitLocals $ 1 + locals,
+                                   text,
+                                   buildMainEnd] where
+  (text, locals) = buildMainContentIR prog val0
 
 buildIR :: Program -> T.Text
 buildIR prog = buildText [buildProlog "PrzykladowaKlasa",
