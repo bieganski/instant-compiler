e5e3139934c93e2de6b546cca2b1abddc1e7fe57
Matuesz <bieganski.m@wp.pl>
2019-10-30 18:31:50 +0100

    will be working, but need to add value token as binop argument

--- src/insc_llvm.hs
+++ src/insc_llvm.hs
@@ -33,7 +33,8 @@ main_end = ["ret i32 0", "}"]
 
 printf_call = "call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([4 x i8], [4 x i8]* @.str, i32 0, i32 0), i32 %%%d)"
 
-epilog = ["attributes #0 = { noinline nounwind optnone uwtable \"correctly-rounded-divide-sqrt-fp-math\"=\"false\" \"disable-tail-calls\"=\"false\" \"less-precise-fpmad\"=\"false\" \"no-frame-pointer-elim\"=\"true\" \"no-frame-pointer-elim-non-leaf\" \"no-infs-fp-math\"=\"false\" \"no-jump-tables\"=\"false\" \"no-nans-fp-math\"=\"false\" \"no-signed-zeros-fp-math\"=\"false\" \"no-trapping-math\"=\"false\" \"stack-protector-buffer-size\"=\"8\" \"target-cpu\"=\"x86-64\" \"target-features\"=\"+fxsr,+mmx,+sse,+sse2,+x87\" \"unsafe-fp-math\"=\"false\" \"use-soft-float\"=\"false\" }",
+epilog = ["declare i32 @printf(i8*, ...) #1",
+          "attributes #0 = { noinline nounwind optnone uwtable \"correctly-rounded-divide-sqrt-fp-math\"=\"false\" \"disable-tail-calls\"=\"false\" \"less-precise-fpmad\"=\"false\" \"no-frame-pointer-elim\"=\"true\" \"no-frame-pointer-elim-non-leaf\" \"no-infs-fp-math\"=\"false\" \"no-jump-tables\"=\"false\" \"no-nans-fp-math\"=\"false\" \"no-signed-zeros-fp-math\"=\"false\" \"no-trapping-math\"=\"false\" \"stack-protector-buffer-size\"=\"8\" \"target-cpu\"=\"x86-64\" \"target-features\"=\"+fxsr,+mmx,+sse,+sse2,+x87\" \"unsafe-fp-math\"=\"false\" \"use-soft-float\"=\"false\" }",
           "attributes #1 = { \"correctly-rounded-divide-sqrt-fp-math\"=\"false\" \"disable-tail-calls\"=\"false\" \"less-precise-fpmad\"=\"false\" \"no-frame-pointer-elim\"=\"true\" \"no-frame-pointer-elim-non-leaf\" \"no-infs-fp-math\"=\"false\" \"no-nans-fp-math\"=\"false\" \"no-signed-zeros-fp-math\"=\"false\" \"no-trapping-math\"=\"false\" \"stack-protector-buffer-size\"=\"8\" \"target-cpu\"=\"x86-64\" \"target-features\"=\"+fxsr,+mmx,+sse,+sse2,+x87\" \"unsafe-fp-math\"=\"false\" \"use-soft-float\"=\"false\" }",
           "!llvm.module.flags = !{!0}",
           "!llvm.ident = !{!1}\n",
@@ -56,26 +57,24 @@ buildText ts = T.intercalate (T.pack "\n") ts
 buildLines :: [String] -> T.Text
 buildLines xs = T.intercalate (T.pack "\n") (map T.pack xs) 
 
-
--- num free variable to be used (numering from 1)
+-- num free variable to be used
 -- and map from variable name to its number and value
 type Id = Integer
 type SState = Map.Map String Id
+-- free variable num to be used, map variable -> its memory num
 type SStateM = State (Id, SState)
 
-s0 :: (Id, SState)
-s0 = (1, Map.empty)
-
-evalBinOp :: Exp -> Exp -> SStateM (T.Text, (Id, Id))
-evalBinOp e1 e2 = do
+computeBinOpIR :: Exp -> Exp -> SStateM (T.Text, (Id, Id))
+computeBinOpIR e1 e2 = do
   (t1, id1) <- evalExp e1
   (t2, id2) <- evalExp e2
   return (buildText [t1, t2], (id1, id2))
 
+
 newId :: SStateM Id
 newId = do
-  (id, _) <- get
-  modify ((+1) . fst)
+  id <- gets fst
+  modify $ \(i, m) -> (i+1, m)
   return id
 
 getBinArgExpPrintf :: Exp -> String
@@ -91,25 +90,35 @@ getBinArgExpPrintf e = case e of
 -- that holds evaluation result
 evalExp :: Exp -> SStateM (T.Text, Id)
 evalExp e = case e of
-  ExpVar x -> do
-    resId <- newId
-    (_, s) <- get
-    let xMemReg = getVariableNum x s
-    return (T.pack $ printf "%%%d = load i32, i32* %%%d, align 4" newId xMemReg,
-            newId)
+  ExpVar (Ident x) -> do
+    xRegId <- newId
+    m <- gets snd
+    return (T.pack "", 1)
+    let res = maybe (error $ "not defined variable usage: " ++ x)
+              (\xMemId -> let load = printf "%%%d = load i32, i32* %%%d, align 4" xRegId xMemId in (T.pack $ load, xRegId))
+              (Map.lookup x m)
+    return res
   ExpLit n -> do
     tmpMemId <- newId
     valRegId <- newId
-    let resLines = [printf "%%%d = alloca i32, align4" tmpMemId,
+    let resLines = [printf "%%%d = alloca i32, align 4" tmpMemId,
                     printf "store i32 %d, i32* %%%d, align 4" n tmpMemId,
                     printf "%%%d = load i32, i32* %%%d, align 4" valRegId tmpMemId]
-    return (buildLines resLines, newId)
-  ExpAdd e1 e2 -> do
-    (t, (id1, id2)) <- evalBinOp e1 e2
-    resId <- newId
-    return (resText, resId) where
-      resText = buildText [t, T.pack $ printf (getBinArgExpPrintf e) resId, id1, id2]
+    return (buildLines resLines, valRegId)
+  ExpAdd e1 e2 -> evalBinOp e e1 e2
+  ExpSub e1 e2 -> evalBinOp e e1 e2
+  ExpDiv e1 e2 -> evalBinOp e e1 e2
+  ExpMul e1 e2 -> evalBinOp e e1 e2
 
+
+-- i.e evalBinOp e e1 e2 for e := ExpAdd e1 e2
+evalBinOp :: Exp -> Exp -> Exp -> SStateM (T.Text, Id)
+evalBinOp e e1 e2 = do
+    (t, (id1, id2)) <- computeBinOpIR e1 e2
+    resId <- newId
+    let op = T.pack $ printf (getBinArgExpPrintf e) resId id1 id2
+    let res = let resText = buildText [t, op] in (resText, resId) 
+    return res 
 
 modifyAssignVariable :: String -> (Id, SState) -> (Id, SState)
 modifyAssignVariable x (num, m) = case Map.lookup x m of
@@ -133,7 +142,7 @@ computeStmtIR stmt = do
       (_, m) <- get
       let xMemAddr = m Map.! x
       let tStore = printf "store i32 %%%d, i32* %%%d, align 4" id xMemAddr
-      let resTextL = [t, tStore]
+      let resTextL = [t, T.pack tStore]
       case used of
         True -> return $ buildText resTextL
         False -> return $ buildText (r:resTextL) where
@@ -152,14 +161,14 @@ computeProgramIR (Prog stmts) = do
 buildMainContentIR :: Program -> (Id, SState) -> T.Text
 buildMainContentIR p s = evalState (computeProgramIR p) s
 
--- limits for locals and stack increased by 1 because of 'getstatic' usage
--- and string argument in main function
--- buildMain :: Program -> T.Text
--- buildMain
+
+s0 :: (Id, SState)
+s0 = (1, Map.empty)
 
 buildIR :: Program -> T.Text
 buildIR prog = buildText [buildLines prolog,
                          buildLines main_begin,
+                         buildMainContentIR prog s0,
                          buildLines main_end,
                          buildLines epilog]
 
